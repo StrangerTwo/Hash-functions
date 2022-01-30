91ffa23a1110e7462835fe6ffeea3c1e281204fc
StrangerTwo <h.volhejn@gmail.com>
2022-01-30 19:03:20 +0100

    Hash function SHA 256

---
+++ Hash-functions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "VendamoD is hardstuck Gold 3";
+            string salt = "TRUEEEEEEEEE";
+
+            string hash = HashString(text, salt);
+
+            Console.WriteLine(text);
+            Console.WriteLine(hash);
+
+            Console.ReadKey();
+        }
+
+        static string HashString(string text, string salt = "")
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return String.Empty;
+            }
+
+            // Uses SHA256 to create the hash
+            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            {
+                // Convert the string to a byte array first, to be processed
+                byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(text + salt);
+                byte[] hashBytes = sha.ComputeHash(textBytes);
+
+                // Convert back to a string, removing the '-' that BitConverter adds
+                string hash = BitConverter
+                    .ToString(hashBytes)
+                    .Replace("-", String.Empty);
+
+                return hash;
+            }
+        }
+    }
+}
